a77eda4aeb5d952254deb1f62b7d84e5c8b367e9
DarthFener <80347286+DarthFener@users.noreply.github.com>
2021-05-14 14:20:06 +0200

    Aggiunto progetto cicli

---
+++ Cicli in Cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cicli_in_Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             while (true) Console.Write("Hello World! ");   // true, in modo da eseguire sempre e comunque il ciclo
+             while (true){
+                Console.Write("Hello World! ");
+           //  parentesi opzionali se istruzione unica
+
+            }
+            do
+            {
+                Console.Write("Hello World");
+            }
+            while (true);
+
+        }
+    }
+}
